7e71f364c73fb3dc2ddc5b563e13f8787764de7a
outs1der <jcsanchez.martinez.2020@gmail.com>
2023-03-04 10:07:07 -0500

    3/4/23

---
+++ c++/punto2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void MilesKilometers(int kilometers){
+	float miles = kilometers/ 0.62137;
+	if (kilogram < 10){
+		cout << endl << "    " << miles << "       " << kilometers << endl ;
+	}
+	else if (kilogram = 10 ) {
+		cout << endl << "    " << miles << "      " << kilometers << endl ;
+	}	
+}
+
+void PoundsKilograms(int pound){
+	float kilogram = pound * 0.45359237;
+	if (kilogram < 10){
+		cout << endl << "    " << pound << "       " << kilogram << endl ;
+	}
+	else if (kilogram >= 10 && kilogram < 100) {
+		cout << endl << "    " << pound << "      " << kilogram << endl ;
+	}	
+}
+
+int menu() {
+	
+	int seleccion;
+	
+	cout << "Seleccione: \n 1. kg a L \n 2. L a K \n";
+	cin >> seleccion;
+
+	return seleccion;
+}
+
+int main(){
+	
+	int seleccion = menu();
+
+	switch(seleccion){
+		case 1 :
+			cout << "Kilograms  Pounds";
+
+			for (int i = 1; i < 200; i++){
+				kilogramsPounds(i);
+			}
+		case 2: 
+			cout << "Pounds  Kilograms";
+
+			for (int i = 20; i < 516; i++){
+				PoundsKilograms(i);
+			}
+	}
+}
