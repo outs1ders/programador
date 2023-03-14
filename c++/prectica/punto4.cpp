76b1aebabf24bcde6c7f0204344f5d0a8ffd2c5c
outs1der <jcsanchez.martinez.2020@gmail.com>
2023-03-14 07:42:17 +0000

    3/14/23

--- c++/prectica/punto4.cpp
+++ c++/prectica/punto4.cpp
@@ -1,2 +1,29 @@
 #include <iostream>
 using namespace std;
+
+int divisor(int num, int i){
+	if (num % i == 0) {
+		return i;
+	}else {
+		return 0;	
+	}
+	return 0;
+}
+
+int main(){
+	int num;
+	
+	cout << "Ingrese el numero: "; cin >> num;
+
+	for (int i = 1 ; i < 100; i++) {
+	
+		int divisores = divisor(num, i);
+
+		if (divisores != 0) {
+			cout << i <<" es un divisor de " << divisores << endl;
+		}
+
+		
+	}
+
+}
